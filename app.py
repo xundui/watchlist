c4eec3181f0e4129484395b88dd701965d5df1dc
xundui <xundui@outlook.com>
2025-11-24 22:01:53 +0800

    Add database

--- app.py
+++ app.py
@@ -1,34 +1,70 @@
-from flask import Flask, url_for, render_template
-from markupsafe import escape
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import String, select
+from sqlalchemy.orm import Mapped, mapped_column
+import click
 
-name = 'Xun'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://xun:_Aa7835381@localhost:3306/data'
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(app, model_class=Base)
+
+@app.cli.command('init-db')  # 注册为命令，传入自定义命令名
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def init_database(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(20))  # 用户名名字
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(60))  # 电影标题
+    year: Mapped[str] = mapped_column(String(4))  # 电影年份
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.drop_all()
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
-
-@app.route('/user/<name>')
-def user_page(name):
-    return f'User: {escape(name)}'
-
-@app.route('/test')
-def test_url_for():
-    print(url_for('hello'))
-    print(url_for('user_page', name='greyli'))
-    print(url_for('user_page', name='peter'))
-    print(url_for('test_url_for'))
-    print(url_for('test_url_for', num=2))
-    return 'Test page'
+    user = db.session.scalar(select(User))  # 读取用户记录
+    movies = db.session.scalars(select(Movie)).all()  # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
